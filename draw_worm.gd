ba4212db4577172d72279cc1617302badc13ad84
SirLich <lrkoehler77@gmail.com>
2024-04-13 10:16:18 +0200

    Get navigation working

--- draw_worm.gd
+++ draw_worm.gd
@@ -2,7 +2,7 @@
 extends Node2D
 
 func _draw():
-	draw_circle(Vector2(0,0), 100, Color.RED)
+	draw_circle(Vector2(0,0), 10, Color.BLACK)
 	
 func _process(delta):
 	queue_redraw()
---
+++ enemy.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+var movement_speed: float = 200.0
+var movement_target_position: Vector2 = Vector2(60.0,180.0)
+
+@export var agent : NavigationAgent2D
+
+func get_player() -> Node2D:
+	return get_tree().get_first_node_in_group("player")
+	
+func _ready():
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	agent.path_desired_distance = 4.0
+	agent.target_desired_distance = 4.0
+	
+	# Make sure to not await during _ready.
+	call_deferred("actor_setup")
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+
+
+	# Set target position
+	movement_target_position = get_player().global_position
+	
+	# Now that the navigation map is no longer empty, set the movement target.
+	set_movement_target(movement_target_position)
+
+func set_movement_target(movement_target: Vector2):
+	agent.target_position = movement_target
+
+func _physics_process(delta):
+	if agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = agent.get_next_path_position()
+
+	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	move_and_slide()
